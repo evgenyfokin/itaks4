a5c57dbcb8dd232483434e3c3e79cec29ca45966
evgenyfokin <fokin_e@inbox.ru>
2023-07-25 21:33:02 +0300

    fix

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const db = {
     url: 'mysql://b06a25ef11eebc:b892f323@us-cdbr-east-06.cleardb.net/heroku_27bd5cee3560c30?reconnect=true'
 };
 
-export const jwtSecret = process.env.JWT_SECRET || 'secret';
+export const jwtSecret = 'secret';
