1e8f50f52e43de329a6aceabaf308fd982ae2672
evgenyfokin <fokin_e@inbox.ru>
2023-07-26 02:18:07 +0300

    fix

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ import jwt from "jsonwebtoken";
 import { jwtSecret } from './config.js';
 import * as authController from './controllers/authentication.js';
 import cors from 'cors'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://taks4f.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: 'include',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}));
+
 app.use(express.json());
+
+app.use(
+    '/api',
+    createProxyMiddleware({
+        target: 'https://chatf-594d6adb216e.herokuapp.com',
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api': '',
+        },
+    })
+);
 
 const checkJwt = (req, res, next) => {
     const token = req.headers['x-access-token'];
@@ -30,7 +50,7 @@ app.post('/updateUserStatus', checkJwt, authController.updateUserStatus);
 app.post('/blockUsers', checkJwt, authController.blockUsers);
 app.get('/users', checkJwt, authController.getUsers);
 app.post('/register', authController.register);
-app.post('/login', authController.login);
+app.post('/', authController.login);
 app.get('/user/:id', checkJwt, authController.getUser);
 
 app.get('/', (req, res) => {
