91b04a496178ed24616f9206b9b2a82d30995bbb
evgenyfokin <fokin_e@inbox.ru>
2023-07-25 17:59:30 +0300

    fix

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 export const db = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Netflix2024',
-    database: 'user_management',
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'Netflix2024',
+    // database: 'user_management',
+    // url: 'mysql://b06a25ef11eebc:b892f323@us-cdbr-east-06.cleardb.net/heroku_27bd5cee3560c30?reconnect=true'
+    url: process.env.CLEARDB_DATABASE_URL
 };
 
-export const jwtSecret = 'secret';
+export const jwtSecret = process.env.JWT_SECRET || 'secret';
